365a1ebdc23f21bfa61d93ebc803067f7b792c92
Fromiron <artless1112@gmail.com>
2020-03-31 13:24:23 +0900

    Component n Props test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function Test({ fav }) {
+  return <h3> I Love { fav } </h3>;
+}
 
 function App() {
   return (
     <div className="App">
-     Hello
+      <h1>Hello</h1>
+      <Test fav="Mushrooms!" />
     </div>
   );
 }
