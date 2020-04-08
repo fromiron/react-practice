39d925bb00f3207b56fc79739a211179fe90639d
Fromiron <artless1112@gmail.com>
2020-04-08 12:49:40 +0900

    class component test

--- src/App.js
+++ src/App.js
@@ -1,64 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h2>I Love {name}</h2>
-      <h5>{rating}/5 </h5>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  add = () => {
+    this.setState((current) => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState((current) => ({count: current.count - 1}));
+  };
 
-const foodIlike = [
-  {
-    id: 1,
-    name: 'Ramen',
-    image: 'https://i.ytimg.com/vi/B8y3SSmz4sg/maxresdefault.jpg',
-    rating: 4.1,
-  },
-
-  {
-    id: 2,
-    name: 'Steak',
-    image: 'https://i.ytimg.com/vi/vmuT33p_6q4/maxresdefault.jpg',
-    rating: 4.1,
-  },
-  {
-    id: 3,
-    name: 'BBQ',
-    image: 'https://i.ytimg.com/vi/B5wIE4zyW5k/maxresdefault.jpg',
-    rating: 4.7,
-  },
-  {
-    id: 4,
-    name: 'Kimchi',
-    image:
-      'https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg',
-    rating: 4.2,
-  },
-];
-
-function renderFood(dish) {
-  return (
-    <Food
-      key={dish.id}
-      name={dish.name}
-      picture={dish.image}
-      rating={dish.rating}
-    ></Food>
-  );
-}
-
-function App() {
-  return <div>{foodIlike.map(renderFood)}</div>;
+  render() {
+    return (
+      <div>
+        <h1>the number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
